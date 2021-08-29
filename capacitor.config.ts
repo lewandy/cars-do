c079b290802f97ff974e7756ddf0847740ee6c25
Lewandy Dilone Bonifacio <lewandydilone1@live.com>
2021-08-29 16:24:33 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'cars-do',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
